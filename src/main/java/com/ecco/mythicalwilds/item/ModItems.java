c2a1ef6bdb3a389f7fda32ad610841f7102f2553
Ecco_notsystem <ember.jacobs0@gmail.com>
2025-08-26 14:20:07 -0700

    changed

---
+++ src/main/java/com/ecco/mythicalwilds/item/ModItems.java
@@ -0,0 +1,8 @@
+package com.ecco.mythicalwilds.item;
+
+import com.ecco.mythicalwilds.MythicalWilds;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MythicalWilds.MOD_ID);
+}
